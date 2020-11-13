fec221dff1bd55dd9c28cce1c80152450c967d16
allimacse <alan.escamillam@gmail.com>
2020-11-12 20:14:28 -0600

    Login

---
+++ src/pages/NotFound/NotFound.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const NotFound = () => (
+  <>
+    <div>
+      <h1>
+        404
+      </h1>
+      <h3>
+        Lo sentimos, pero la página que buscas no existe.
+      </h3>
+    </div>
+  </>
+);
+
+export default NotFound;
